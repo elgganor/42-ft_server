2dad805faf84594ef70b183f00a811730eacfe5b
mrouabeh <mrouabeh@student.42.fr>
2019-12-17 03:57:04 +0100

    Gestion des services

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM debian:buster
 RUN apt-get update
 RUN apt-get install -y nginx php php7.3-fpm vim mariadb-server php7.3-mysql
 RUN mkdir -p /var/www/localhost
-COPY ./src/wordpress-5.3.1.tar.gz /var/www/localhost/
+COPY ./src/wordpress-5.3.1.tar.gz /root
 COPY ./src/server_config /etc/nginx/sites-available/
 COPY ./src/script.sh /root
 COPY ./src/phpMyAdmin-4.9.2-all-languages.tar.gz /root
--- src/script.sh
+++ src/script.sh
@@ -1,17 +1,20 @@
-cd /var/www/localhost
+#Mise en place des fichiers du serveur
+cd /root
+#phpmyadmin
+tar zxvf phpMyAdmin-4.9.2-all-languages.tar.gz
+rm phpMyAdmin-4.9.2-all-languages.tar.gz
+cp ./config.inc.php ./phpMyAdmin-4.9.2-all-languages/
+mv phpMyAdmin-4.9.2-all-languages/ /var/www/localhost/phpmyadmin/
+#wordpress
 tar zxvf wordpress-5.3.1.tar.gz
 rm -f wordpress-5.3.1.tar.gz
-cp /root/wp-config.php ./wordpress
+cp ./wp-config.php ./wordpress
+cp -r wordpress/* /var/www/localhost/
 cd /etc/nginx
 rm -f ./sites-enabled/default
 cd ./sites-enabled
 ln -s ../sites-available/server_config .
-cd /root
-tar zxvf phpMyAdmin-4.9.2-all-languages.tar.gz
-rm phpMyAdmin-4.9.2-all-languages.tar.gz
-mv phpMyAdmin-4.9.2-all-languages/ /var/www/localhost/wordpress/phpmyadmin/
 cd /
-cp /root/config.inc.php /var/www/localhost/wordpress/phpmyadmin/
 service nginx start
 service php7.3-fpm start
 service mysql start
